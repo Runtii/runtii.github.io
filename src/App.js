3a20ffc48f4454c123944f44a568744804802250
Runtii <65392934+Runtii@users.noreply.github.com>
2023-08-10 20:03:57 +0200

    site under development

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import construction from "./images/under construction.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={construction} width="300px" />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="About" element={<About />} />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,7 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <>
-      <h1>Home page</h1>
-      <Link to="/about">About</Link>
-      <p>Page is still under development</p>
+      <p>Page is under development. Please come later.</p>
     </>
   );
 }
