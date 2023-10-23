1bbfb56275be424d06a0db399f6fb2eb76206215
Runtii <albertto.ap@gmail.com>
2023-10-23 18:45:56 +0200

    removed error with babel plugin
    also made further changes with project

--- src/App.js
+++ src/App.js
@@ -4,13 +4,10 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import PersonalLinks from "./pages/Personal links";
 
-let i = 10;
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h3>{i}</h3>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/About" element={<About />} />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,11 @@ export default function Home() {
   return (
     <>
       <h1>Home page</h1>
-      <Link to="/about">About</Link>
-      <Link to="/PersonalLinks">Personal links</Link>
-      <p>Page is still under development</p>
+      <div className="main">
+        <Link to="/about">About</Link>
+        <Link to="/PersonalLinks">Personal links</Link>
+        <p>Page is still under development</p>
+      </div>
     </>
   );
 }
